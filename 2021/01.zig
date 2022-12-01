8ef0a501999d469d2b1724a126f284a77f459ce4
GethDW <gethwilliams@googlemail.com>
2022-12-01 14:51:10 +0000

    2021 day 1

---
+++ 2021/01.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const example = @embedFile("01_example.txt");
+const input = @embedFile("01.txt");
+
+pub fn part1() !u32 {
+    var count: u32 = 0;
+    var lines = std.mem.tokenize(u8, input, "\n");
+    var previous = try std.fmt.parseInt(u32, lines.next().?, 10);
+    while (lines.next()) |line| {
+        const current = try std.fmt.parseInt(u32, line, 10);
+        if (current > previous) count += 1;
+        previous = current;
+    }
+    return count;
+}
+
+pub fn part2() !u32 {
+    var count: u32 = 0;
+    var lines = std.mem.tokenize(u8, input, "\n");
+    var window: [3]u32 = undefined;
+    var prev: u32 = 0;
+    for (window) |*item| {
+        item.* = try std.fmt.parseInt(u32, lines.next().?, 10);
+        prev += item.*;
+    }
+    while (lines.next()) |line| {
+        std.mem.rotate(u32, &window, 1);
+        window[2] = try std.fmt.parseInt(u32, line, 10);
+        var current: u32 = 0;
+        for (window) |item| current += item;
+        if (current > prev) count += 1;
+        prev = current;
+    }
+    return count;
+}
