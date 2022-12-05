1141f8ed6dc78b6b5c32ed855f757ad0ab0a164c
GethDW <gethwilliams@googlemail.com>
2022-12-05 16:15:16 +0000

    2022 day 5: improve
    singly linked list for less memory usage
    stack allocate first 64 nodes and end up dynamically allocating 0

--- 2022/05.zig
+++ 2022/05.zig
@@ -1,18 +1,21 @@
 const input = @embedFile("05.txt");
 
 const std = @import("std");
+const mem = std.mem;
+
+const Stack = std.SinglyLinkedList(u8);
+const NodePool = std.SegmentedList(Stack.Node, 64);
 
 fn parseState(
     str: []const u8,
-    nodes: *std.SegmentedList(std.TailQueue(u8).Node, 0),
-    allocator: std.mem.Allocator,
-) ![9]std.TailQueue(u8) {
-    var stacks = [1]std.TailQueue(u8){.{}} ** 9;
-    var rows = std.mem.tokenize(u8, str, "\n");
+    nodes: *NodePool,
+    allocator: mem.Allocator,
+) ![9]Stack {
+    var stacks = [1]Stack{.{}} ** 9;
+    var rows = mem.splitBackwards(u8, str, "\n");
+    _ = rows.next();
     while (rows.next()) |line| {
-        if (line[1] == '1') break;
         const row = @ptrCast([*]const [4]u8, line)[0 .. line.len / 4 + 1];
-        std.debug.assert(row.len == 9);
         for (row) |e, i| {
             if (e[1] != ' ') {
                 const node = try nodes.addOne(allocator);
@@ -31,7 +34,7 @@ const Inst = struct {
 };
 fn parseInstruction(inst: []const u8) !Inst {
     var ret: Inst = undefined;
-    var iter = std.mem.split(u8, inst, " ");
+    var iter = mem.tokenize(u8, inst, " ");
     _ = iter.next();
     ret.count = try std.fmt.parseInt(usize, iter.next().?, 10);
     _ = iter.next();
@@ -41,52 +44,51 @@ fn parseInstruction(inst: []const u8) !Inst {
     return ret;
 }
 
-pub fn part1(allocator: std.mem.Allocator) ![]const u8 {
-    const setup_end = std.mem.indexOf(u8, input, "\n\n").?;
-    var instructions = std.mem.tokenize(u8, input[setup_end + 2 ..], "\n");
-    var nodes = std.SegmentedList(std.TailQueue(u8).Node, 0){};
+pub fn part1(allocator: mem.Allocator) ![9]u8 {
+    const setup_end = mem.indexOf(u8, input, "\n\n").?;
+
+    var nodes = NodePool{};
     defer nodes.deinit(allocator);
     var stacks = try parseState(input[0..setup_end], &nodes, allocator);
 
+    var instructions = mem.tokenize(u8, input[setup_end + 2 ..], "\n");
     while (instructions.next()) |inst| {
         const in = try parseInstruction(inst);
-        const from: *std.TailQueue(u8) = &stacks[in.from];
-        const to: *std.TailQueue(u8) = &stacks[in.to];
+        const from = &stacks[in.from];
+        const to = &stacks[in.to];
         var i: usize = 0;
-        while (i < in.count) : (i += 1) to.append(from.pop().?);
+        while (i < in.count) : (i += 1) to.prepend(from.popFirst().?);
     }
 
-    var ret = std.ArrayListUnmanaged(u8){};
-    for (stacks) |*stack| {
-        if (stack.pop()) |node| try ret.append(allocator, node.data);
+    var ret: [9]u8 = undefined;
+    for (stacks) |stack, i| {
+        ret[i] = if (stack.first) |node| node.data else ' ';
     }
-
-    return try ret.toOwnedSlice(allocator);
+    return ret;
 }
 
-pub fn part2(allocator: std.mem.Allocator) ![]const u8 {
-    const setup_end = std.mem.indexOf(u8, input, "\n\n").?;
-    var instructions = std.mem.tokenize(u8, input[setup_end + 2 ..], "\n");
-    var nodes = std.SegmentedList(std.TailQueue(u8).Node, 0){};
+pub fn part2(allocator: mem.Allocator) ![9]u8 {
+    const setup_end = mem.indexOf(u8, input, "\n\n").?;
+
+    var nodes = NodePool{};
     defer nodes.deinit(allocator);
     var stacks = try parseState(input[0..setup_end], &nodes, allocator);
 
+    var instructions = mem.tokenize(u8, input[setup_end + 2 ..], "\n");
     while (instructions.next()) |inst| {
         const in = try parseInstruction(inst);
-        const from: *std.TailQueue(u8) = &stacks[in.from];
-        const to: *std.TailQueue(u8) = &stacks[in.to];
+        const from = &stacks[in.from];
+        const to = &stacks[in.to];
 
-        var tmp = std.TailQueue(u8){};
+        var tmp = Stack{};
         var i: usize = 0;
-        while (i < in.count) : (i += 1) tmp.append(from.pop().?);
-        i = 0;
-        while (i < in.count) : (i += 1) to.append(tmp.pop().?);
+        while (i < in.count) : (i += 1) tmp.prepend(from.popFirst().?);
+        while (tmp.popFirst()) |node| to.prepend(node);
     }
 
-    var ret = std.ArrayListUnmanaged(u8){};
-    for (stacks) |*stack| {
-        if (stack.pop()) |node| try ret.append(allocator, node.data);
+    var ret: [9]u8 = undefined;
+    for (stacks) |stack, i| {
+        ret[i] = if (stack.first) |node| node.data else ' ';
     }
-
-    return try ret.toOwnedSlice(allocator);
+    return ret;
 }
