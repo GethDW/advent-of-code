beef03d17d1ba2cf0f0475e3c28e233151776614
GethDW <gethwilliams@googlemail.com>
2023-05-03 14:09:52 +0100

    2022, day 08, part 1

--- 2022/08.zig
+++ 2022/08.zig
@@ -1,36 +1,50 @@
 const std = @import("std");
 
-pub fn part1(input: []const u8, allocator: std.mem.Allocator) !void {
-    const rows: []const []const u8 = blk: {
-        var grid = std.ArrayList([]const u8).init(allocator);
+pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
+    const width = blk: {
+        var lines = std.mem.tokenize(u8, input, "\n");
+        break :blk lines.next().?.len;
+    };
+    const height = width;
+    const rows: []const u8 = blk: {
+        var grid = std.ArrayList(u8).init(allocator);
         defer grid.deinit();
         var lines = std.mem.tokenize(u8, input, "\n");
-        while (lines.next()) |line| try grid.append(line);
+        while (lines.next()) |line| {
+            try grid.appendSlice(line);
+        }
         break :blk try grid.toOwnedSlice();
     };
+    defer allocator.free(rows);
+    std.debug.assert(rows.len == width * height);
 
-    var count: u32 = 0;
-    trees: for (rows, 0..) |row, x| {
-        for (row, 0..) |_, y| {
-            const height = rows[x][y];
-            if (blk: {
-                for (rows[0..x]) |h| {
-                    if (h >= height) break :blk false;
-                }
-                break :blk true;
-            }) {
-                count += 1;
-                continue :trees;
-            }
-            if (blk: {
-                for (rows[x + 1 ..]) |h| {
-                    if (h >= height) break :blk false;
-                }
-                break :blk true;
-            }) {
-                count += 1;
-                continue :trees;
-            }
-        }
-    }
+    var visible = try std.DynamicBitSet.initEmpty(allocator, width * height);
+    defer visible.deinit();
+
+    for (1..width - 1) |x| for (1..height - 1) |y| {
+        const i = y * width + x;
+        const current = rows[i];
+        const is_visible = blk: {
+            for (x + 1..width) |z| {
+                if (current <= rows[y * width + z]) break;
+            } else break :blk true;
+
+            for (0..x) |z| {
+                if (current <= rows[y * width + z]) break;
+            } else break :blk true;
+
+            for (y + 1..height) |z| {
+                if (current <= rows[z * width + x]) break;
+            } else break :blk true;
+
+            for (0..y) |z| {
+                if (current <= rows[z * width + x]) break;
+            } else break :blk true;
+
+            break :blk false;
+        };
+        if (is_visible) visible.set(i);
+    };
+
+    return visible.count() + 2 * width + 2 * height - 4;
 }
