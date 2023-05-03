96674af24758a44ce8a405145aba1939bb3d505c
GethDW <gethwilliams@googlemail.com>
2023-05-03 14:18:55 +0100

    2022, day 08, part 1: code golf

--- 2022/08.zig
+++ 2022/08.zig
@@ -1,50 +1,41 @@
 const std = @import("std");
 
+// we assume:
+//    - the input is equal length lines of single digit numbers.
+//    - each line ends with a single '\n'.
+// we offset indexing of input by +(y * width) to account for the newline.
+// we offset indexing of the bit set by -1 for x and y to avoid storing edges.
 pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    const width = blk: {
-        var lines = std.mem.tokenize(u8, input, "\n");
-        break :blk lines.next().?.len;
-    };
-    const height = width;
-    const rows: []const u8 = blk: {
-        var grid = std.ArrayList(u8).init(allocator);
-        defer grid.deinit();
-        var lines = std.mem.tokenize(u8, input, "\n");
-        while (lines.next()) |line| {
-            try grid.appendSlice(line);
-        }
-        break :blk try grid.toOwnedSlice();
-    };
-    defer allocator.free(rows);
-    std.debug.assert(rows.len == width * height);
+    const width = std.mem.indexOfScalar(u8, input, '\n').?;
 
-    var visible = try std.DynamicBitSet.initEmpty(allocator, width * height);
+    // we don't store the edges
+    var visible = try std.DynamicBitSet.initEmpty(allocator, (width - 1) * (width - 1));
     defer visible.deinit();
 
-    for (1..width - 1) |x| for (1..height - 1) |y| {
-        const i = y * width + x;
-        const current = rows[i];
+    for (1..width - 1) |x| for (1..width - 1) |y| {
+        const current = input[y * (width + 1) + x];
         const is_visible = blk: {
             for (x + 1..width) |z| {
-                if (current <= rows[y * width + z]) break;
+                if (current <= input[y * (width + 1) + z]) break;
             } else break :blk true;
 
             for (0..x) |z| {
-                if (current <= rows[y * width + z]) break;
+                if (current <= input[y * (width + 1) + z]) break;
             } else break :blk true;
 
-            for (y + 1..height) |z| {
-                if (current <= rows[z * width + x]) break;
+            for (y + 1..width) |z| {
+                if (current <= input[z * (width + 1) + x]) break;
             } else break :blk true;
 
             for (0..y) |z| {
-                if (current <= rows[z * width + x]) break;
+                if (current <= input[z * (width + 1) + x]) break;
             } else break :blk true;
 
             break :blk false;
         };
-        if (is_visible) visible.set(i);
+        if (is_visible) visible.set((y - 1) * width + (x - 1));
     };
 
-    return visible.count() + 2 * width + 2 * height - 4;
+    // we didn't check the edges so we add them after the fact
+    return visible.count() + 4 * width - 4;
 }
